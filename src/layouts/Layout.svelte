d5a3b11763ff9a5d2e00f4a6151dcc767f98451e
jinified <jinified@gmail.com>
2021-02-17 20:51:31 +0800

    Init elderjs template

---
+++ src/layouts/Layout.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let templateHtml, settings;
+</script>
+
+<style>
+  :global(h1) {
+    font-style: italic;
+  }
+  .container {
+    max-width: 900px;
+    margin: 0 auto;
+    padding: 1rem;
+  }
+
+  :root {
+    --balloon-color: #06395a;
+    --balloon-font-size: 14px;
+  }
+</style>
+
+<svelte:head>
+  <link rel="stylesheet" href="/style.css" />
+  <!-- You can remove this balloon it is just for hover effects -->
+  <link rel="stylesheet" href="https://unpkg.com/balloon-css/balloon.min.css" />
+</svelte:head>
+<div class="container">
+  {@html templateHtml}
+</div>
